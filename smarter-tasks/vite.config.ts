ada71f8a7bbcdebd2b9ccc220b5c9503f09819da
Gurmu Gessese <gurmugt@gmail.com>
2023-10-07 12:09:47 +0530

    Committing level 9 by adding features of react suspense,lazy loadingand PWA Config for Vite

--- smarter-tasks/vite.config.ts
+++ smarter-tasks/vite.config.ts
@@ -1,7 +1,55 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default ({ mode }) => {
+  return defineConfig({
+    build: {
+      outDir: "build",
+    },
+    plugins: [
+      react(),
+      VitePWA({
+        devOptions: {
+          enabled: true,
+        },
+        manifest: {
+          name: "Smarter Tasks application",
+          short_name: "Smarter Tasks",
+          icons: [
+            {
+              src: "/favicon.ico",
+              sizes: "64x64 32x32 24x24 16x16",
+              type: "image/x-icon",
+            },
+            {
+              src: "/favicon-16x16.png",
+              type: "image/png",
+              sizes: "16x16",
+            },
+            {
+              src: "/favicon-32x32.png",
+              type: "image/png",
+              sizes: "32x32",
+            },
+            {
+              src: "/pwa-192x192.png",
+              type: "image/png",
+              sizes: "192x192",
+            },
+            {
+              src: "/pwa-512x512.png",
+              type: "image/png",
+              sizes: "512x512",
+              purpose: "any maskable",
+            },
+          ],
+          theme_color: "#AAF",
+        },
+      }),
+    ],
+    define: {
+      "process.env.NODE_ENV": `"${mode}"`,
+    },
+  });
+};
