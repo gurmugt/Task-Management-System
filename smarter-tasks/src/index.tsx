e265a94e4ec515fcffee1ecd8909e1f0c6de3c1c
Gurmu Gessese <gurmugt@gmail.com>
2023-10-07 12:41:00 +0530

    Adding features of suspense and lazy loading to the ProjectDetails and MemberList Components

---
+++ smarter-tasks/src/index.tsx
@@ -0,0 +1,10 @@
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { ThemeProvider } from "./context/theme";
+import App from "./App";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <ThemeProvider>
+    <App />
+  </ThemeProvider>
+);
